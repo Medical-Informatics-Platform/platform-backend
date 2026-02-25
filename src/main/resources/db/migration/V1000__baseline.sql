dad996303011f5a170ba00c13269835ef64b9b36
KFilippopolitis <kostasfilippop@gmail.com>
2026-02-25 12:42:36 +0200

    db(flyway): replace historical migration chain with new baseline

---
+++ src/main/resources/db/migration/V1000__baseline.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS "user" (
+    username CHARACTER VARYING PRIMARY KEY,
+    subject_id CHARACTER VARYING,
+    fullname CHARACTER VARYING,
+    email CHARACTER VARYING,
+    agree_nda BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE IF NOT EXISTS "experiment" (
+    uuid UUID PRIMARY KEY,
+    name TEXT,
+    created_by_username CHARACTER VARYING REFERENCES "user"(username),
+    status TEXT CHECK (status IN ('error', 'pending', 'success')),
+    result TEXT,
+    finished TIMESTAMP WITHOUT TIME ZONE,
+    algorithm TEXT,
+    algorithm_id TEXT,
+    created TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated TIMESTAMP WITHOUT TIME ZONE,
+    shared BOOLEAN DEFAULT FALSE,
+    viewed BOOLEAN DEFAULT FALSE,
+    mip_version TEXT
+);
